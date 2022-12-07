cb15850a17e04edcbf4057e259db2ec06fa8c6ea
Quang Anh <89133721+doquanganh99zz@users.noreply.github.com>
2022-12-07 08:06:36 +0700

    fix anotation
    them anotation service controller

--- clothesshop/src/main/java/shop/clothesshop/controller/AppController.java
+++ clothesshop/src/main/java/shop/clothesshop/controller/AppController.java
@@ -1,4 +1,11 @@
 package shop.clothesshop.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/product1.0")
+@CrossOrigin(allowedHeaders = "*", origins = "*")
 public class AppController {
 }
--- clothesshop/src/main/java/shop/clothesshop/services/AdminServices.java
+++ clothesshop/src/main/java/shop/clothesshop/services/AdminServices.java
@@ -1,6 +1,7 @@
 package shop.clothesshop.services;
 
+import org.springframework.stereotype.Service;
 import shop.clothesshop.services.iservices.IAdminServices;
-
+@Service
 public class AdminServices implements IAdminServices {
 }
