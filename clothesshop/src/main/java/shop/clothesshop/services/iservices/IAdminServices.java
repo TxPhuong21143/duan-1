f87fb6e68e404066d9189f9b428b1611518024ff
jwigwijgs <duongnmnph28124@fpt.edu.vn>
2022-12-09 21:54:37 +0700

    em update modul voucher
    update modul voucher

--- clothesshop/src/main/java/shop/clothesshop/services/iservices/IAdminServices.java
+++ clothesshop/src/main/java/shop/clothesshop/services/iservices/IAdminServices.java
@@ -2,8 +2,10 @@ package shop.clothesshop.services.iservices;
 
 
 import shop.clothesshop.entities.Product;
+import shop.clothesshop.entities.Sales;
 import shop.clothesshop.entities.adminrespon.AnalysisObject;
 import shop.clothesshop.entities.adminrespon.BillAnalysis;
+import shop.clothesshop.entities.adminrespon.SalesObject;
 
 import java.util.List;
 
@@ -13,4 +15,9 @@ public interface IAdminServices {
     public List<BillAnalysis> getAllBillByType(int type);
     public List<Product> searchInShop(String search);
 
+    public SalesObject getAllSales();
+
+    public void createVoucher(Sales data);
+
+    public void closeVoucher(int idVoucher);
 }
