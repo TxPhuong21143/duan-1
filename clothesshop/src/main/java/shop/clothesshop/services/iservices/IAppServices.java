52c25081baabb0f4f06a954aeb6dadb8fc3dec88
Quang Anh <89133721+doquanganh99zz@users.noreply.github.com>
2022-12-11 17:55:15 +0700

    update controller
    them controller cho app

--- clothesshop/src/main/java/shop/clothesshop/services/iservices/IAppServices.java
+++ clothesshop/src/main/java/shop/clothesshop/services/iservices/IAppServices.java
@@ -1,18 +1,22 @@
 package shop.clothesshop.services.iservices;
 
+import shop.clothesshop.entities.AccountBag;
 import shop.clothesshop.entities.AccountShipContact;
+import shop.clothesshop.entities.Bill;
+import shop.clothesshop.entities.Product;
 import shop.clothesshop.entities.requestobject.CreateAccountData;
+import shop.clothesshop.entities.requestobject.OrderData;
 import shop.clothesshop.entities.requestobject.RePass;
 import shop.clothesshop.entities.requestobject.RemakeAccountRequest;
-import shop.clothesshop.entities.responobject.AccountCustom;
-import shop.clothesshop.entities.responobject.RePassRespon;
+import shop.clothesshop.entities.responobject.*;
+
+import java.util.List;
 
 public interface IAppServices {
 
     public AccountCustom checkLogin(String useName, String userPass);
 
     public AccountCustom getAccountContacts(int accountId);
-
 
     public CreateAccountData createAccountAndAccountShipContact(CreateAccountData accountData);
 
@@ -21,4 +25,35 @@ public interface IAppServices {
     public RemakeAccountRequest remakeAcountInfo(RemakeAccountRequest accountRemake);
 
     public RePassRespon rePass(RePass rePassData);
+    public List<Product> getProductHome();
+
+    public ProductDetail getProductId(int accountId);
+
+    public List<ShowAccountBag> getProductBagByAccountID(int accountId);
+
+    public List<OrderObject> getOrderObjectByAccountId(int accountId);
+
+    public AccountBag addProduct2Bag(int accountId, int productId, int quantity);
+
+    public AccountBag deleteAccountBag(int accountBagId);
+
+    public CreateOrder createOrder(Integer[] listIdAccountBag);
+
+    public AccountBag updateAccountBag(Integer[] accountBagData);
+
+    public OrderData createBill(OrderData orderData);
+
+    public Bill cancelBill(Integer billId, Integer type);
+
+    public AccountShipContact removeAccountShipContact(Integer idAccountShipContact);
+
+    public List<Product> nextPage(int page);
+
+    public List<Product> dressCategory();
+
+    public List<Product> panCategory();
+
+    public List<Product> shirtCategory();
+
+    public List<Product> searchProduct(String searchInput);
 }
