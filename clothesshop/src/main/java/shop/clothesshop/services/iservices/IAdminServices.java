fb851b96db046e185431c2be2b3f335c0fb308df
Quang Anh <89133721+doquanganh99zz@users.noreply.github.com>
2022-12-11 17:47:36 +0700

    update admin services
    them modul cho san pham va ban tai quay

--- clothesshop/src/main/java/shop/clothesshop/services/iservices/IAdminServices.java
+++ clothesshop/src/main/java/shop/clothesshop/services/iservices/IAdminServices.java
@@ -1,4 +1,56 @@
 package shop.clothesshop.services.iservices;
 
+import org.springframework.web.multipart.MultipartFile;
+import shop.clothesshop.entities.AccountShipContact;
+import shop.clothesshop.entities.Bill;
+import shop.clothesshop.entities.Product;
+import shop.clothesshop.entities.adminrequest.CreateAndRemakePropertyData;
+import shop.clothesshop.entities.adminrequest.CreateProductData;
+import shop.clothesshop.entities.adminrequest.HoldBillRequestPayBill;
+import shop.clothesshop.entities.adminrespon.HoldBill;
+import shop.clothesshop.entities.adminrespon.PrintBillData;
+import shop.clothesshop.entities.adminrespon.PropertyObject;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
 public interface IAdminServices {
+    public PropertyObject getPropertyObject();
+
+    public CreateAndRemakePropertyData createProperty(CreateAndRemakePropertyData createAndRemakePropertyData);
+
+    public CreateAndRemakePropertyData remakeProperty(CreateAndRemakePropertyData createAndRemakePropertyData);
+
+    public void addProduct(MultipartFile file1, MultipartFile file2, CreateProductData data) throws IOException;
+
+    public List<Product> getByListId(Set<Integer> listId);
+
+    public List<Product> getAllProduct();
+
+    public Product remakeProduct(Product p);
+
+    public Bill payBillInShop(AccountShipContact contact, int idEmployee, int billId);
+
+    public Bill createBillInShop(int idEmployee);
+
+    public void removeHodlingBillDetail(int idBillDetail);
+
+    public List<Bill> getAllHoldingBill();
+
+    public Product findProductById(Integer idProduct);
+
+    public HoldBill getAllBillDetailOfBill(Integer idBill);
+
+    public void addProduct2BillDetail(Integer idProduct, Integer idBill);
+
+    public void deleteBillDetail(Integer idBillDetail);
+
+    public void updateQuantityBillDetail(Integer idBillDetail, Integer quantity);
+
+    public Double sumBill(Integer billId);
+
+    public void payHoldBill(HoldBillRequestPayBill holdBillRequestPayBill);
+
+    public PrintBillData printBillData(Integer idBill);
 }
